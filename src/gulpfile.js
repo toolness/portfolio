0f1f5a5894d9e7be38d3ce416d95141eacc65019
Atul Varma <varmaa@gmail.com>
2015-07-28 17:10:41 -0400

    add s3 task

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,6 +1,9 @@
 let path = require('path');
 let express = require('express');
 let gulp = require('gulp');
+let gutil = require('gutil');
+let gzip = require('gulp-gzip');
+let s3 = require('gulp-s3');
 let yamlFront = require('yaml-front-matter');
 let marked = require('marked');
 let through2 = require('through2');
@@ -98,4 +101,33 @@ gulp.task('watch', BUILD_TASKS, cb => {
   app.listen(8080, function() {
     console.log("Development server is listening on port 8080.");
   });
+});
+
+gulp.task('s3', BUILD_TASKS, function() {
+  let key = process.env.AWS_ACCESS_KEY;
+  let secret = process.env.AWS_SECRET_KEY;
+
+  gutil.log('NODE_ENV is ' + process.env.NODE_ENV + '.');
+
+  if (!key || !secret) {
+    throw new Error('Please set AWS_ACCESS_KEY and AWS_SECRET_KEY ' +
+    'in your environment.');
+  }
+
+  // WARNING: Even if deploying to S3 fails, no errors will be raised.
+  // https://github.com/nkostelnik/gulp-s3/issues/47
+
+  return gulp.src('./dist/**')
+    .pipe(gzip())
+    .pipe(s3({
+      key: key,
+      secret: secret,
+      bucket: process.env.AWS_BUCKET || 'portfolio.toolness.org',
+      region: process.env.AWS_REGION || 'us-east-1'
+    }, {
+      gzippedOnly: true,
+      headers: {
+        'Cache-Control': 'max-age=600, public'
+      }
+    }));
 });
