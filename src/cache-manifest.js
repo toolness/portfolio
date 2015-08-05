2b3e2e922b1a5085b3f659d41848b8d7073a5568
Atul Varma <varmaa@gmail.com>
2015-08-05 09:15:36 -0400

    cache ionicons properly

--- src/cache-manifest.js
+++ src/cache-manifest.js
@@ -1,6 +1,8 @@
 let path = require('path');
 let walk = require('walk');
 
+const IONICONS_VERSION = '2.0.0';
+
 module.exports = (distDir) => {
   let error = null;
   let lines = [
@@ -19,6 +21,8 @@ module.exports = (distDir) => {
             .replace(/\\/g, '/') + '/';
           if (fileStats.name != 'index.html')
             relPath += fileStats.name;
+          if (/ionicons\.(eot|svg|ttf|woff)$/.test(fileStats.name))
+            relPath += '?v=' + IONICONS_VERSION;
           if (relPath != '/')
             relPath = '/' + relPath;
           lines.push(relPath);
